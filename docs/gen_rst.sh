2aa3ed121b0fd8176583efdce9d3af9bd86b90da
Paul Triantafyllou <trianta@research.att.com>
2018-02-27 15:58:16 -0500

    upgraded setup.py metadata for pypi
    Change-Id: Ie55aafe99715d1816a5a160a4075c7ddbac89ba3
    Signed-off-by: Paul Triantafyllou <trianta@research.att.com>

--- docs/gen_rst.sh
+++ docs/gen_rst.sh
@@ -2,9 +2,8 @@
 
 header=".. THIS FILE WAS GENERATED. DO NOT EDIT.\n"
 
-for mdfile in $(ls *.md); do
-	filename=$(basename $mdfile .md)
-	outfile="$filename.rst"
+for mdfile in $(find . -name '*.md'); do
+	outfile="${mdfile%.*}.rst"
 	pandoc --from=markdown --to=rst --output=$outfile $mdfile;
 	echo -e $header | cat - $outfile > temp && mv temp $outfile
 done
