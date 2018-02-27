7f036e47bad1d0ba1f0c5566e91a863faf37bb5c
Paul Triantafyllou <trianta@research.att.com>
2018-02-27 15:58:16 -0500

    upgraded setup.py metadata for pypi
    Incremental changes
    * added abspath to locating doc files
    * added setup.py to flake8 check
    * case sensitive file system issue
    * trying to remove manifest (jk)
    
    Change-Id: Ie55aafe99715d1816a5a160a4075c7ddbac89ba3
    Signed-off-by: Paul Triantafyllou <trianta@research.att.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
-import os
+from os.path import dirname, abspath, join as path_join
 from setuptools import setup, find_packages
 
 
 # extract __version__ from version file. importing will lead to install failures
-setup_dir = os.path.dirname(__file__)
-with open(os.path.join(setup_dir, 'acumos', '_version.py')) as file:
+SETUP_DIR = abspath(dirname(__file__))
+DOCS_DIR = path_join(SETUP_DIR, 'docs')
+
+with open(path_join(SETUP_DIR, 'acumos', '_version.py')) as file:
     globals_dict = dict()
     exec(file.read(), globals_dict)
     __version__ = globals_dict['__version__']
+
+
+def _long_descr():
+    '''Yields the content of documentation files for the long description'''
+    for file in ('README.rst', 'tutorial/index.rst', 'release-notes.rst', 'contributing.rst'):
+        doc_path = path_join(DOCS_DIR, file)
+        with open(doc_path) as f:
+            yield f.read()
 
 
 setup(
@@ -17,7 +27,8 @@ setup(
     packages=find_packages(),
     author='Paul Triantafyllou',
     author_email='trianta@research.att.com',
-    description=('Acumos client library'),
+    description='Acumos client library for building and pushing Python models',
+    long_description='\n'.join(_long_descr()),
     setup_requires=['pytest-runner'],
     install_requires=['typing',
                       'protobuf',
@@ -28,4 +39,16 @@ setup(
                       'filelock'],
     python_requires='>=3.4',
     license='Apache License 2.0',
+    url='https://www.acumos.org/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    keywords='acumos machine learning model modeling artificial intelligence ml ai',
 )
