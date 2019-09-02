01ba690ea31b5ee61cc43a89e917b932e13a0b78
JingLu5 <lvjing5@huawei.com>
2019-09-02 11:29:59 +0800

    [WIP] add raw format support
    Issue-Id: ACUMOS-2712
    
    Add raw format support for acmos-python-client:
    Allow user specified raw data types
    
    This is a part of Acumos ONAP integration work.
    
    Signed-off-by: JingLu5 <lvjing5@huawei.com>
    Change-Id: Icc4acc52a7ab64125c086a1c40c9ae8d7320cfc5

---
+++ examples/raw_example.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+'''
+Dumps an example model for illustrating acumos_model_runner usage
+'''
+
+from acumos.session import AcumosSession
+from acumos.modeling import Model, new_type
+
+# allow users to specify a "raw" bytes type. no protobuf message is generated here
+Image = new_type(bytes, 'Image', {'dcae_input_name': 'a', 'dcae_output_name': 'a'}, 'example description')
+
+
+def image_func(image: Image) -> Image:
+    '''Return an image'''
+    return Image(image)
+
+
+if __name__ == '__main__':
+    '''Main'''
+    model = Model(imgae_func=image_func)
+
+    session = AcumosSession()
+    session.dump(model, 'example', '.')
