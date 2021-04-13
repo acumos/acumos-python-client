0d2c92558076e2b5fda48603f1290fcb9fc960ff
PhilippDo <philippe.dooze@orange.com>
2021-04-13 11:25:27 +0200

    Fix failed with python 3_9
    Issue-Id: ACUMOS-4323
    Change-Id: I7979f9cd158d4fbc5c4038ad8d78a50e6a172bcf
    Signed-off-by: PhilippDo <philippe.dooze@orange.com>

--- acumos-package/acumos/_version.py
+++ acumos-package/acumos/_version.py
@@ -16,4 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============LICENSE_END=========================================================
-__version__ = '0.9.9'
+__version__ = '1.0.0'
